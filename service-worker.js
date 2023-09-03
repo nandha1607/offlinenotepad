27758894646256349f63388e41eadc94a4e06839
nandhakumar.kp <nandhakumar.kp@zohocorp.com>
2023-09-03 23:04:01 +0530

    service worker

--- service-worker.js
+++ service-worker.js
@@ -3,19 +3,19 @@ self.addEventListener('install', function(event) {
       caches.open('notes-cache').then(function(cache) {
         return cache.addAll([
           '/',
-          'index.html',
-          'js/script.js',
-          'css/styles.css',
-          'manifest.json',
-          'libs/js/bootstrap.min.js', 
-          'libs/js/jquery-3.6.0.min.js',
-          'libs/css/bootstrap.min.css',
-          'libs/fontawesome-free-6.4.2-web/css/all.min.css',
-          'libs/fontawesome-free-6.4.2-web/webfonts/fa-solid-900.woff2',
-          'libs/fontawesome-free-6.4.2-web/LICENSE.txt',
-          'img/logo.png',
-          'img/github-icon.png',
-          'img/icon.png'
+          '/index.html',
+          '/js/script.js',
+          '/css/styles.css',
+          '/manifest.json',
+          '/libs/js/bootstrap.min.js', 
+          '/libs/js/jquery-3.6.0.min.js',
+          '/libs/css/bootstrap.min.css',
+          '/libs/fontawesome-free-6.4.2-web/css/all.min.css',
+          '/libs/fontawesome-free-6.4.2-web/webfonts/fa-solid-900.woff2',
+          '/libs/fontawesome-free-6.4.2-web/LICENSE.txt',
+          '/img/logo.png',
+          '/img/github-icon.png',
+          '/img/icon.png'
           // ... Add other files to cache ...
         ]);
       })
