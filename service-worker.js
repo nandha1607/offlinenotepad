646a03c6ebae425b737bd630503553d7d1e84fcb
nandhakumar.kp <nandhakumar.kp@zohocorp.com>
2023-09-03 23:05:23 +0530

    service worker

--- service-worker.js
+++ service-worker.js
@@ -22,11 +22,46 @@ self.addEventListener('install', function(event) {
     );
   });
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open('notes-cache').then(function(cache) {
+        return cache.addAll([
+          // ... List of resources to cache ...
+        ]);
       })
     );
   });
+  
+  self.addEventListener('activate', function(event) {
+    // You can handle cache cleanup or version management here.
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(cachedResponse) {
+        if (cachedResponse) {
+          // Cache hit, return the cached response
+          return cachedResponse;
+        }
+  
+        // No cache match, fetch the request from the network
+        return fetch(event.request).then(function(response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+  
+          // Clone the response to store it in the cache
+          var responseToCache = response.clone();
+  
+          caches.open('notes-cache').then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+  
+          return response;
+        });
+      })
+    );
+  });
+  
   
