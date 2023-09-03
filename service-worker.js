8738231152994ac985021a0f3603f040006f5b8e
nandhakumar.kp <nandhakumar.kp@zohocorp.com>
2023-09-04 01:53:21 +0530

    fetch event added

--- service-worker.js
+++ service-worker.js
@@ -73,3 +73,12 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+
+// This part of the code fetches resources from the network when available
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
