79ab44114ff50df442b7a0c2c787c96d7cb07cf5
nandhakumar.kp <nandhakumar.kp@zohocorp.com>
2023-09-04 02:00:39 +0530

    error handling

--- service-worker.js
+++ service-worker.js
@@ -22,63 +22,45 @@ const cachedFiles = [
 
 // Service Worker Installation
 self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll(cachedFiles);
-    })
-  );
-});
-
-// Service Worker Activation (Cleanup or Version Management)
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          // Delete outdated caches (if any)
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Service Worker Fetch (Network Request Handling)
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(cachedResponse) {
-      if (cachedResponse) {
-        // Cache hit, return the cached response
-        return cachedResponse;
-      }
-
-      // No cache match, fetch the request from the network
-      return fetch(event.request).then(function(response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200) {
-          return response;
-        }
-
-        // Clone the response to store it in the cache
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
+    event.waitUntil(
+      caches.open(CACHE_NAME).then(function(cache) {
+        return cache.addAll(cachedFiles).catch(function(error) {
+          console.error('Failed to cache one or more resources:', error);
         });
-
-        return response;
-      });
-    })
-  );
-});
-
-// This part of the code fetches resources from the network when available
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
-    })
-  );
-});
+      })
+    );
+  });
+  
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(cachedResponse) {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request).then(function(response) {
+          if (!response || response.status !== 200) {
+            return response;
+          }
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        });
+      })
+    );
+  });
