cfb3c90931cfc2d84400203127dda1f129fc6dd9
nandhakumar.kp <nandhakumar.kp@zohocorp.com>
2023-09-04 15:47:00 +0530

    cache changes

--- service-worker.js
+++ service-worker.js
@@ -51,78 +51,42 @@ self.addEventListener('activate', function(event) {
 // Service Worker Fetch (Network Request Handling)
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    // Check for requests to "https://nandha1607.github.io"
-    // and redirect them to "https://nandha1607.github.io/offlinenotepad"
-    redirectToOfflineNotepad(event)
+    caches.match(event.request).then(function(cachedResponse) {
+      let online = navigator.onLine;
+
+      // Check network connectivity
+      if (!online) {
+        if (cachedResponse) {
+          // Cache hit, return the cached response
+          return cachedResponse;
+        } else {
+          // Network is offline, and no cached response available
+          // Respond with a custom offline page or an error page
+          return new Response('You are offline.');
+        }
+      }
+
+      // Network is online, fetch from the network
+      return fetch(event.request)
+        .then(function(response) {
+          // Check if we received a valid response
+          if (!response || response.status !== 200) {
+            return response;
+          }
+
+          // Clone the response to store it in the cache
+          var responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        })
+        .catch(function(error) {
+          // Network request failed, respond with a custom error page
+          return new Response('Network request failed.');
+        });
+    })
   );
 });
-
-// Function to redirect requests to "https://nandha1607.github.io" to "https://nandha1607.github.io/offlinenotepad"
-function redirectToOfflineNotepad(event) {
-  const requestUrl = new URL(event.request.url);
-
-  if (requestUrl.origin === 'https://nandha1607.github.io') {
-    // Redirect the request to "https://nandha1607.github.io/offlinenotepad"
-    const redirectedUrl = 'https://nandha1607.github.io/offlinenotepad' + requestUrl.pathname;
-  
-    return fetch(redirectedUrl, { mode: 'no-cors' })
-      .then(function(response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200) {
-          return response;
-        }
-
-        // Clone the response to store it in the cache
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      })
-      .catch(function(error) {
-        // Network request failed, respond with a custom error page
-        return new Response('Network request failed.');
-      });
-  }
-
-  // If the request doesn't match the condition, proceed with the regular caching logic
-  return caches.match(event.request).then(function(cachedResponse) {
-    let online = navigator.onLine;
-
-    // Check network connectivity
-    if (!online) {
-      if (cachedResponse) {
-        // Cache hit, return the cached response
-        return cachedResponse;
-      } else {
-        // Network is offline, and no cached response available
-        // Respond with a custom offline page or an error page
-        return new Response('You are offline.');
-      }
-    }
-
-    // Network is online, fetch from the network
-    return fetch(event.request)
-      .then(function(response) {
-        // Check if we received a valid response
-        if (!response || response.status !== 200) {
-          return response;
-        }
-
-        // Clone the response to store it in the cache
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME).then(function(cache) {
-          cache.put(event.request, responseToCache);
-        });
-
-        return response;
-      })
-      .catch(function(error) {
-        // Network request failed, respond with a custom error page
-        return new Response('Network request failed.');
-      });
-  });
-}
